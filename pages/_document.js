18b6ec29e0471128583f5f7bd389e65138fefaa6
Dima Odintsov <dimaodin1@icloud.com>
2022-04-16 19:27:35 +0300

    #1

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Oswald:wght@100;200;300;400;500;600;700;800&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Oswald:wght@100;200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
         </Head>
